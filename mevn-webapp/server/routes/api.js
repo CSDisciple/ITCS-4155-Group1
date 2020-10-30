e19c37823c9ae9b4a77ce0c2ca1f467741af3c11
Oleks <oleksstasyev@gmail.com>
2020-10-30 11:25:41 -0400

    Commented out gpt3

--- mevn-webapp/server/routes/api.js
+++ mevn-webapp/server/routes/api.js
@@ -17,38 +17,38 @@ router.get('/', async (req, res) => {
 async function main() {
 
   try {
-  //   await client.connect();
-  //   await listDatabases(client);
-  //   await create(client, [
-  //     {
-  //         name: "Infinite Views",
-  //         summary: "Modern home with infinite views from the infinity pool",
-  //         property_type: "House",
-  //         bedrooms: 5,
-  //         bathrooms: 4.5,
-  //         beds: 5
-  //     },
-  //     {
-  //         name: "Private room in London",
-  //         property_type: "Apartment",
-  //         bedrooms: 1,
-  //         bathroom: 1
-  //     },
-  //     {
-  //         name: "Beautiful Beach House",
-  //         summary: "Enjoy relaxed beach living in this house with a private beach",
-  //         bedrooms: 4,
-  //         bathrooms: 2.5,
-  //         beds: 7,
-  //         last_review: new Date()
-  //     }
+    await client.connect();
+    await listDatabases(client);
+    await create(client, [
+      {
+          name: "Infinite Views",
+          summary: "Modern home with infinite views from the infinity pool",
+          property_type: "House",
+          bedrooms: 5,
+          bathrooms: 4.5,
+          beds: 5
+      },
+      {
+          name: "Private room in London",
+          property_type: "Apartment",
+          bedrooms: 1,
+          bathroom: 1
+      },
+      {
+          name: "Beautiful Beach House",
+          summary: "Enjoy relaxed beach living in this house with a private beach",
+          bedrooms: 4,
+          bathrooms: 2.5,
+          beds: 7,
+          last_review: new Date()
+      }
 
-  // ]);
-  //   await read(client, 'Infinite Views');
-  //   await update(client, 'Infinite Views', {bedrooms: 6, beds:8,});
-  //   await deleteListing(client, 'Cozy Cottage');
-  await gpt3();
-  await gpt3res();
+  ]);
+    await read(client, 'Infinite Views');
+    await update(client, 'Infinite Views', {bedrooms: 6, beds:8,});
+    await deleteListing(client, 'Cozy Cottage');
+  // await gpt3();
+  // await gpt3res();
   } catch (e) {
     console.error(e);
   }
