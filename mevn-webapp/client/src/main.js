ecefbb8ed34d93cec5bfe172acc1f2c8fe2cda47
Oleks <oleksstasyev@gmail.com>
2020-11-24 21:20:08 -0500

    Vue.js conversion

--- mevn-webapp/client/src/main.js
+++ mevn-webapp/client/src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import MediumEditor from "vuejs-medium-editor";
-import BootstrapVue from "bootstrap-vue";
-// import "bootstrap/dist/css/bootstrap.min.css"
-// import "bootstrap-vue/dist/bootstrap-vue.css"
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+require('@/assets/css/bootstrap.css')
+// // Install BootstrapVue
+// Vue.use(BootstrapVue)
+// // Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
 
-Vue.use(BootstrapVue);
+
 Vue.component("medium-editor", MediumEditor);
 Vue.config.productionTip = false;
 
