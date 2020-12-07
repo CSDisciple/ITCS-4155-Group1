da3e7de84fd81509419526ce581d789e37fd4d5e
Oleks <oleksstasyev@gmail.com>
2020-12-07 14:01:44 -0500

    Main App

---
+++ interactive-storyteller/client/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {},
+  methods: {
+    onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log("Name: " + profile.getName());
+      console.log("Image URL: " + profile.getImageUrl());
+      console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    },
+  },
+};
+</script>
+
+<style scoped>
+.g-signin2 {
+  border: 1px solid black;
+  height: 2em;
+  color: black;
+}
+</style>
--- mevn-webapp/client/src/assets/vuejs-medium-editor/src/Editor.vue
+++ mevn-webapp/client/src/assets/vuejs-medium-editor/src/Editor.vue
@@ -1,37 +1,42 @@
 <template>
-    <div>
-        <!-- Editor Mode -->
-        <div class="medium-editor-container" v-if="!readOnly">
-            <insert-embed v-if="editor"
-                :uploadUrl="options.uploadUrl"
-                :uploadUrlHeader="options.uploadUrlHeader"
-                :file_input_name="options.file_input_name"
-                :imgur_bool="options.imgur"
-                :onChange="triggerChange"
-                :editorRef="$refs.editor"
-                :editor="editor"
-                v-on:uploaded="uploadedCallback"></insert-embed>
-            <list-handler v-if="editor"
-                :editor="editor"
-                :onChange="triggerChange"></list-handler>
-            <div class="editor"
-                v-bind:class="editorClass"
-                v-html="prefill"
-                ref="editor">
-            </div>
-        </div>
-        <!-- Read Only Mode -->
-        <read-mode v-if="readOnly" :content="prefill"></read-mode>
+  <div>
+    <!-- Editor Mode -->
+    <div class="medium-editor-container" v-if="!readOnly">
+      <insert-embed
+        v-if="editor"
+        :uploadUrl="options.uploadUrl"
+        :uploadUrlHeader="options.uploadUrlHeader"
+        :file_input_name="options.file_input_name"
+        :imgur_bool="options.imgur"
+        :onChange="triggerChange"
+        :editorRef="$refs.editor"
+        :editor="editor"
+        v-on:uploaded="uploadedCallback"
+      ></insert-embed>
+      <list-handler
+        v-if="editor"
+        :editor="editor"
+        :onChange="triggerChange"
+      ></list-handler>
+      <div
+        class="editor"
+        v-bind:class="editorClass"
+        v-html="prefill"
+        ref="editor"
+      ></div>
     </div>
+    <!-- Read Only Mode -->
+    <read-mode v-if="readOnly" :content="prefill"></read-mode>
+  </div>
 </template>
 
 <script>
-import MediumEditor from 'medium-editor';
-import InsertEmbed from './libs/InsertEmbed';
-import ListHandler from './libs/ListHandler';
-import ReadMode from './libs/ReadMode';
-import _ from 'underscore';
-import hljs from 'highlight.js';
+import MediumEditor from "medium-editor";
+import InsertEmbed from "./libs/InsertEmbed";
+import ListHandler from "./libs/ListHandler";
+import ReadMode from "./libs/ReadMode";
+import _ from "underscore";
+import hljs from "highlight.js";
 
 export default {
   name: "medium-editor",
@@ -41,18 +46,29 @@ export default {
       defaultOptions: {
         forcePlainText: false,
         placeholder: {
-          text: "Write something great!!"
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         },
         uploadUrl: "https://api.imgur.com/3/image",
-        uploadUrlHeader: {'Authorization': 'Client-ID db856b43cc7f441'},
+        uploadUrlHeader: { Authorization: "Client-ID db856b43cc7f441" },
         file_input_name: "image",
         imgur: true,
         toolbar: {
-          buttons: ["bold", "italic", "quote", "h1", "h2", "h3", "h4", "h5", "anchor" ]
-        }
+          buttons: [
+            "bold",
+            "italic",
+            "quote",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "anchor",
+          ],
+        },
       },
       hasContent: false,
-        autoLink: true
+      autoLink: true,
     };
   },
   props: ["options", "onChange", "prefill", "readOnly"],
@@ -61,18 +77,18 @@ export default {
       return _.extend(this.defaultOptions, this.options);
     },
     editorClass() {
-        return {
-            'has-content': this.hasContent
-        }
-    }
+      return {
+        "has-content": this.hasContent,
+      };
+    },
   },
   components: {
     InsertEmbed,
     ListHandler,
-    ReadMode
+    ReadMode,
   },
   mounted() {
-      this.addClassToPre();
+    this.addClassToPre();
     if (!this.readOnly) {
       this.createElm();
     }
@@ -94,7 +110,7 @@ export default {
       this.editor.destroy();
     },
     triggerChange() {
-        this.addClassToPre() ;
+      this.addClassToPre();
       const content = this.editor.getContent();
       setTimeout(() => {
         if (/<[a-z][\s\S]*>/i.test(content)) {
@@ -112,16 +128,16 @@ export default {
       // console.log("callback")
       this.$emit("uploaded", url);
     },
-      addClassToPre() {
-          hljs.configure({useBR: true});
-          document.querySelectorAll('pre').forEach((block) => {
-              hljs.highlightBlock(block);
-              block.setAttribute("spellcheck", "false");
-          });
-      }
+    addClassToPre() {
+      hljs.configure({ useBR: true });
+      document.querySelectorAll("pre").forEach((block) => {
+        hljs.highlightBlock(block);
+        block.setAttribute("spellcheck", "false");
+      });
+    },
   },
   destroyed() {
     this.destroyElm();
-  }
+  },
 };
 </script>
